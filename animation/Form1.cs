ff6cdf857e722105c65959a5008bfdb12632f834
cute73998 <cute73998@gmail.com>
2024-08-21 16:02:12 +0700

    Add project files.

---
+++ animation/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace animation
+{
+    public partial class picture1 : Form
+    {
+        private VideoCapture capture;
+        private Mat frame;
+        private Bitmap image;
+        public picture1()
+        {
+            InitializeComponent();
+            capture = new VideoCapture(0); // Kết nối với webcam, 0 là ID mặc định của webcam
+            frame = new Mat();  // Khởi tạo một Mat để lưu trữ khung hình từ webcam
+            Application.Idle += ProcessFrame; // Xử lý khung hình khi ứng dụng rảnh
+        }
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            capture.Read(frame);  // Đọc một khung hình từ webcam
+            if (!frame.Empty())  // Kiểm tra xem khung hình có trống không
+            {
+                image = BitmapConverter.ToBitmap(frame);  // Chuyển đổi Mat thành Bitmap
+                pictureBox1.Image = image;  // Hiển thị hình ảnh trong PictureBox
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            capture.Release();  // Giải phóng tài nguyên khi đóng ứng dụng
+        }
+    }
+
+}
